72df84876166b328b7c2a52421d69673a1f1981e
chinesetingxie <54350929+chinesetingxie@users.noreply.github.com>
2021-04-05 18:01:38 +0800

    hanziv2

--- js/hanzi.js
+++ js/hanzi.js
@@ -5,6 +5,8 @@ var nMistakes = 0;
 var lesson;
 var words = [];
 var wordString = "";
+var totalWords;
+var c;
 
 $(document).ready(function() 
 {		
@@ -28,16 +30,19 @@ $(document).ready(function()
 		for (i = 0; i < result.words.length; i++)
 		{
 			words.push(result.words[i]);
-			wordString += result.words[i];
+			wordString += result.words[i] + " ";
 		}
-	
+		
 		//shuffle(words);
 		nextQuestion();
 	});
 	
 	$('.check').click(function() 
 	{		
-		nextQuestion();		
+		n++;
+		if (n >= words.length)
+			n = 0;
+		nextQuestion();	
 	});
 	
 	$('.prev').click(function() 
@@ -61,20 +66,18 @@ $(document).ready(function()
 	});	
 });
 
-function boldChar(n)
+function boldChar()
 {
-	var s = wordString;
 	var newS = "";
-	var i;
-	for (i = 0; i < wordString.length; i++)
+	for (i = 0; i < words.length; i++)
 	{
 		if (i == n)
 		{
-			newS += "<strong class='highlight'>" + s.substring(i, i + 1) + "</strong>";
+			newS += "<strong class='highlight'>" + words[i] + "</strong> ";
 		}
 		else
 		{
-			newS += s.substring(i, i + 1)
+			newS += words[i] + " ";
 		}
 	}
 	return newS;
@@ -85,74 +88,98 @@ function nextQuestion()
 	$("#gif").hide();
 	$(".check").hide();
 	
+	$("#characters").empty();
+	
 	getGif();
 	
-	$(".words").html(boldChar(n));
+	c = 0;
+	
+	var totalWords = words[n].length;
+	
+	for (i = 0; i < words[n].length; i++)
+	{
+		
+		$('#characters').append('<div id="word' + i + '"></div>');
+		
+		var writer = HanziWriter.create('word' + i, words[n][i], 
+		{
+			width: 350,
+			height: 350,
+			showCharacter: false,
+			showOutline: learn,
+			showHintAfterMisses: 3,
+			padding: 0
+		});
+		
+		$('#word' + i).append('<p><span class="badge badge-info char-info' + words[n][i] + '"></span><span class="badge badge-warning char-warning' + words[n][i] + '"></span><span class="badge badge-success char-success' + words[n][i] + '"></span><span class="badge badge-danger char-danger' + words[n][i] + '"></span></p>')
+		
+		writer.quiz({
+			onMistake: function(strokeData) 
+			{
+				nMistakes = strokeData.totalMistakes;
+				$(".char-info" + strokeData.character).text('Oh no! you made ' + strokeData.totalMistakes + ' mistakes so far.');
+				$(".char-info" + strokeData.character).show();
+
+				/*console.log("You've made " + strokeData.mistakesOnStroke + " mistakes on this stroke so far");
+				console.log("You've made " + strokeData.totalMistakes + " total mistakes on this quiz");
+				console.log("There are " + strokeData.strokesRemaining + " strokes remaining in this character");*/
+			},
+			onCorrectStroke: function(strokeData) 
+			{
+				if (strokeData.strokesRemaining > 0)
+				{
+					$(".char-info" + strokeData.character).text('Yes!!! There are ' + strokeData.strokesRemaining + ' strokes remaining.');
+					$(".char-info" + strokeData.character).show();
+				}
+				/*console.log('Yes!!! You got stroke ' + strokeData.strokeNum + ' correct!');
+				console.log('You made ' + strokeData.mistakesOnStroke + ' mistakes on this stroke');
+				console.log("You've made " + strokeData.totalMistakes + ' total mistakes on this quiz');
+				console.log('There are ' + strokeData.strokesRemaining + ' strokes remaining in this character');*/
+			},
+			onComplete: function(summaryData) 
+			{
+				$(".char-info" + summaryData.character).hide();
+				if (nMistakes >= 5)
+				{
+					$(".char-danger" + summaryData.character).text('Try harder!');
+					$(".char-danger" + summaryData.character).show();
+				} 
+				else if (nMistakes >= 1)
+				{
+					$(".char-warning" + summaryData.character).text('Almost there!');
+					$(".char-warning" + summaryData.character).show();
+				}
+				else if (nMistakes == 0)
+				{
+					$(".char-success" + summaryData.character).text('Perfect!');
+					$(".char-success" + summaryData.character).show();
+				}
+					
+				/*console.log('You did it! You finished drawing ' + summaryData.character);
+				console.log('You made ' + summaryData.totalMistakes + ' total mistakes on this quiz');*/
+				c++;
+				
+				if (c >= totalWords)
+				{
+			
+					$(".check").show();
+					//$(".badge").hide();
+					$("#gif").show();
+				}
+
+			}
+		});
+		
+	}
+	
+	$(".words").html(boldChar());
 	
 	$(".badge").hide();
 	$(".check").hide();
 	
-	$("#character-target-div").empty();
+	
 	
 	nMistakes = 0;
-	
-	var writer = HanziWriter.create('character-target-div', words[n], 
-	{
-		width: 350,
-		height: 350,
-		showCharacter: false,
-		showOutline: learn,
-		showHintAfterMisses: 3,
-		padding: 0
-	});
-	
-	writer.quiz({
-		onMistake: function(strokeData) 
-		{
-			nMistakes = strokeData.totalMistakes;
-			$(".badge-info").text('Oh no! you made ' + strokeData.totalMistakes + ' mistakes so far.');
-			$(".badge-info").show();
-			/*console.log("You've made " + strokeData.mistakesOnStroke + " mistakes on this stroke so far");
-			console.log("You've made " + strokeData.totalMistakes + " total mistakes on this quiz");
-			console.log("There are " + strokeData.strokesRemaining + " strokes remaining in this character");*/
-		},
-		onCorrectStroke: function(strokeData) 
-		{
-			$(".badge-info").text('Yes!!! There are ' + strokeData.strokesRemaining + ' strokes remaining.');
-			$(".badge-info").show();
-			/*console.log('Yes!!! You got stroke ' + strokeData.strokeNum + ' correct!');
-			console.log('You made ' + strokeData.mistakesOnStroke + ' mistakes on this stroke');
-			console.log("You've made " + strokeData.totalMistakes + ' total mistakes on this quiz');
-			console.log('There are ' + strokeData.strokesRemaining + ' strokes remaining in this character');*/
-		},
-		onComplete: function(summaryData) 
-		{
-			$(".badge").hide();
-			$("#gif").show();
-			if (nMistakes >= 5)
-			{
-				$(".badge-danger").text('Try harder!');
-				$(".badge-danger").show();
-			} 
-			else if (nMistakes >= 1)
-			{
-				$(".badge-warning").text('Almost there!');
-				$(".badge-warning").show();
-			}
-			else if (nMistakes == 0)
-			{
-				$(".badge-success").text('Perfect!');
-				$(".badge-success").show();
-			}
-				
-			/*console.log('You did it! You finished drawing ' + summaryData.character);
-			console.log('You made ' + summaryData.totalMistakes + ' total mistakes on this quiz');*/
-			n++;
-			if (n >= words.length)
-				n = 0;
-			$(".check").show();
-		}
-	});
 	
 	$(".audio").attr('src', "./" + lesson + "/hanzi/" + words[n] + ".mp3");	
 }
